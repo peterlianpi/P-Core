a9bbfed1e04aefd989ff413f56a7ce82a1c50d6e
Peter <36624809+peterlianpi@users.noreply.github.com>
2025-07-23 01:19:56 +0630

    remove unneed file

--- app/(protected)/hello-world/page.tsx
+++ app/(protected)/hello-world/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import EditLessonBookFormPage from "@/features/school-management/features/lesson-books/components/update-lesson-book";
-
 export default function StudentDynamicFormPage() {
   return (
-    <div className="">
-      <EditLessonBookFormPage />
+    <div className="flex items-center justify-center mx-auto">
+      <p className="text-5xl font-semibold"> Hello World!</p>
     </div>
   );
 }
